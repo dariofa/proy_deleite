b3bc47052cce17aed0a3fd6535caedd74016e2dd
caparra92 <caparra1992@gmail.com>
2017-08-02 23:00:53 -0500

    Se agrega metodo hasMany para otrosEgresos
    SE agrega metodo para otrosEgresos con relacion hasMany

---
+++ app/Caja.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Caja extends Model
+{
+    protected $table = "cajas";
+
+    protected $fillable = [
+    		'nombre',
+			'saldo_actual',
+			'saldo_anterior'	
+		];
+	public function egresos_caja(){
+		
+		return $this->belongsToMany('App\ProductoBodega','egresos_caja')->withPivot('producto_bodega_id','caja_id','user_id','valor')->withTimestamps();
+	}
+	public function ingresos_caja(){
+		
+		return $this->belongsToMany('App\Pedido','ingresos_caja')->withPivot('pedido_id','caja_id','user_id','valor')->withTimestamps();
+	}
+	public function otros_egresos_caja(){
+		
+		return $this->hasMany('App\OtrosEgresosCaja');
+	}
+}
