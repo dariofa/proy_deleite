659c0400420dcde09c74dc0fa6e86a66b4e3be40
caparra92 <caparra1992@gmail.com>
2017-08-02 23:06:21 -0500

    Se agregan las vistas relacionadas a la variable $cajas y $clientes

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -14,6 +14,8 @@ class ComposerServiceProvider extends ServiceProvider
     public function boot()
     {
         View::composer(['admin.recetas.create','admin.recetas.edit'],'App\Http\ViewComposers\RecetasComposer');
+        View::composer(['admin.bodega.productos.create','admin.bodega.productos.index','admin.cajas.otrosEgresos.create'],'App\Http\ViewComposers\ProductosComposer');
+        View::composer(['admin.pedidos.create','admin.pedidos.index'],'App\Http\ViewComposers\PedidosComposer');
     }
 
     /**
