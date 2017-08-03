1d8ce61f5b3395772a75f72692460d1180b43fc5
caparra92 <caparra1992@gmail.com>
2017-08-02 23:04:26 -0500

    Modificacion de productosComposer
    se agrega variable $cajas a la vista cajas/otrosEgresos/create

---
+++ app/Http/ViewComposers/ProductosComposer.php
@@ -0,0 +1,21 @@
+<?php 
+namespace App\Http\ViewComposers;
+
+use Illuminate\View\View;
+use App\Caja;
+
+
+/**
+* 
+*/
+class ProductosComposer
+{
+	
+	public function compose(View $view)
+	{
+		$cajas = Caja::pluck('nombre','id');	
+		$view->with(['cajas'=>$cajas]);
+	}
+}
+
+
